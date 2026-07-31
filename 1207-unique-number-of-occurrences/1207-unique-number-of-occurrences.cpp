216cd6b434c219fbea71afb14adb2b61919b73e5
Abhijay Singh  Panwar <abhijaysinghpanwar822@gmail.com>
2026-08-01 00:25:29 +0530

    Time: 0 ms (100%), Space: 12.2 MB (17.42%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        set<int> s;
+        for(int i=0;i<arr.size();i++) mp[arr[i]]++;
+        for(auto pair: mp) s.insert(pair.second);
+        if(s.size()==mp.size()) return true;
+        return false;
+    }
+};
